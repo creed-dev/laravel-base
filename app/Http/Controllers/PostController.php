96ec60de01bd74c7ae00557409a2e70a11266f1a
Ilya Chupin <ilya.chupin@itwis.org>
2021-11-15 14:07:10 +0700

    lesson 22 completed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,8 +49,9 @@ class PostController extends Controller
     public function edit(Post $post)
     {
         $categories = Category::all();
+        $tags = Tag::all();
 
-        return view('post.edit', compact('post', 'categories'));
+        return view('post.edit', compact('post', 'categories', 'tags'));
     }
 
     public function update(Post $post)
@@ -58,10 +59,14 @@ class PostController extends Controller
         $data = request()->validate([
             'title' => 'string',
             'text' => 'string',
-            'category_id' => ''
+            'category_id' => '',
+            'tags' => ''
         ]);
+        $tags = $data['tags'];
+        unset($data['tags']);
 
         $post->update($data);
+        $post->tags()->sync($tags);
 
         return redirect()->route('posts.show', $post);
     }
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -7,11 +7,13 @@
         @method('patch')
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="Title" value="{{ $post->title }}">
+            <input type="text" name="title" class="form-control" id="title" placeholder="Title"
+                   value="{{ $post->title }}">
         </div>
         <div class="mb-3">
             <label for="text" class="form-label">Text</label>
-            <textarea rows="10" name="text" class="form-control" id="text" placeholder="Text">{{ $post->text }}</textarea>
+            <textarea rows="10" name="text" class="form-control" id="text"
+                      placeholder="Text">{{ $post->text }}</textarea>
         </div>
         <div class="mb-3">
             <label for="category" class="form-label">Category</label>
@@ -20,6 +22,18 @@
                     <option
                         {{ $category->id === $post->category_id ? 'selected' : '' }}
                         value="{{ $category->id }}">{{ $category->title }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="tags" class="form-label">Tags</label>
+            <select class="form-control" multiple id="category" name="tags[]">
+                @foreach($tags as $tag)
+                    <option
+                        @foreach($post->tags as $postTag)
+                        {{ $tag->id === $postTag->id ? 'selected' : '' }}
+                        @endforeach
+                        value="{{ $tag->id }}">{{ $tag->title }}</option>
                 @endforeach
             </select>
         </div>
