77540885eb5536280aaf607b6132dbd926955bbb
creed-dev <creed-dev@internet.ru>
2022-03-04 18:55:21 +0700

    lesson 28 completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Category::factory(10)->create();
+        User::factory()->create();
         // \App\Models\User::factory(10)->create();
     }
 }
